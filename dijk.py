980375dff73e77f68dbcf3bb68e7eaf3f0413d34
Martin <marcindanlowski@gmail.com>
2017-08-10 18:34:35 +0200

    Add files via upload

--- dijk.py
+++ dijk.py
@@ -1,33 +1,57 @@
-# Dijkstra's algorithm implementation
-# M Danlowski (github.com/mdanlowski)
-'''
-		  _6_(A)_1__
-	     /	  | 	\
-(START)-<     3      >-(FINISH)
-		 \_2__|__5__/
-		 	 (B)				note: b>a but not a>b
-'''
+processed = []
+
+def next_lowest_cost_node( costs_table ):
+    lowest_cost = float("inf")
+    lowest_node = None
+    for node in costs_table:
+        current_cost = costs_table[node]
+        if current_cost < lowest_cost and node not in processed:
+            lowest_cost = current_cost
+            lowest_node = node
+    # print(lowest_node)
+    return lowest_node
+         
 
 graph = {
-	"start" : { "a" : 6 , "b" : 2 },
-	"a"		: { "finish" : 1 },
-	"b"		: { "a" : 3, "finish" : 5 } 
+	'start' : { 'a' : 6, 'b' : 2 },
+	'a' : { 'finish' : 1 },
+	'b' : { 'a' : 3, 'finish' : 5 },
+	'finish' : {}
 }
 costs = {
-	"a" : 6,
-	"b" : 2
+	'a' : 6,
+	'b' : 2,
+	'finish' : float('inf')
 }
 parents = {
-	"a" : "start",
-	"b" : "start"
+	'start' : None,
+	'a' : 'start',
+	'b' : 'start',
+	'finish' : None
 }
 
-# print(graph)
-rootkeys = graph.keys()
-# print(len(rootkeys))
+node = next_lowest_cost_node(costs)
+while node is not None:
+	cost = costs[node]
+	nebrs = graph[node]
+	for n in nebrs.keys():
+	    new_cost = cost + nebrs[n]
+	    if new_cost < costs[n]:
+	        costs[n] = new_cost
+	        parents[n] = node
+	processed.append(node)
+	node = next_lowest_cost_node(costs)   
 
-for key in rootkeys:
-	print(key, graph[key])
-	for k in graph[key].keys():
-		print(k, graph[key][k])
+path = ''
+elem = 'finish'
+while elem != None:
+	if elem == 'start': path += elem
+	else: path += (elem + " <-- ")
+	elem = parents[elem]
+ 
+print("Path:  " + path)
+print("Cost: ", costs['finish'])
 
+# print(processed)
+# print(costs)
+# print(parents)
